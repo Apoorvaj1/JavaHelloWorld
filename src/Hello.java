de5aaa385a2ea456d8105fe165e9705633b03781
apoorv jain <apoorv127@gmail.com>
2023-12-09 15:22:20 +0530

    Second commit

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ package src;
 
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Hello World1");
+        short a = 128;
+        System.out.println("Byte = "+(a+2));
     }
 }
